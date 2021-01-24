80eddaf45393316465ebfdeebc49c9cbdc72bbdd
dominikvrbic <dominikvrbic01@gmail.com>
2021-01-24 21:30:34 +0100

    init

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,14 +8,6 @@ module.exports = {
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
-    [
-      '@snowpack/plugin-build-script',
-      {
-        cmd: 'postcss',
-        input: ['.css'],
-        output: ['.css'],
-      },
-    ],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
